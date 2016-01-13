0607c27e8b04682bffcf00d98f186d6cc6986b53
Arne Binder <arne.b.binder@gmail.com>
2016-01-13 17:36:29 +0100

    fixed folder structure

--- filter-controller/src/main/java/eu/freme/bservices/filtercontroller/persistence/FilterRepository.java
+++ filter-controller/src/main/java/eu/freme/bservices/filtercontroller/persistence/FilterRepository.java
@@ -1,7 +1,6 @@
 package eu.freme.bservices.filtercontroller.persistence;
 
 import eu.freme.common.persistence.repository.OwnedResourceRepository;
-import org.springframework.stereotype.Repository;
 
 /**
  * Created by Arne on 11.12.2015.
