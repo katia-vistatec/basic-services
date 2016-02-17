877b9471dbec8398a7dc054c49d1ea567135b746
Arne Binder <arne.b.binder@gmail.com>
2016-02-17 19:15:43 +0100

    added comment

--- controllers/pipelines/src/test/java/eu/freme/bservices/controllers/pipelines/PipelinesControllerTest.java
+++ controllers/pipelines/src/test/java/eu/freme/bservices/controllers/pipelines/PipelinesControllerTest.java
@@ -60,7 +60,7 @@ public class PipelinesControllerTest {
     }
 
 
-    @Ignore
+    @Ignore // wait for https://github.com/freme-project/FREMECommon/issues/17
     @Test
     public void testPipelineRepository() throws JsonProcessingException {
         long pipelineCountBefore = pipelineDAO.count();
