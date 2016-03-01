bcb821d10e5ee22d056f65dcd0ba9990cf4f7d61
Arne Binder <arne.b.binder@gmail.com>
2016-03-01 20:17:56 +0100

    #21 (migrated changes, added exception catching in controller, additional error tests)

--- filters/postprocessing/src/main/java/eu/freme/bservices/filters/postprocessing/PostprocessingFilter.java
+++ filters/postprocessing/src/main/java/eu/freme/bservices/filters/postprocessing/PostprocessingFilter.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import com.mashape.unirest.request.HttpRequestWithBody;
 import eu.freme.common.conversion.rdf.RDFConstants;
 import eu.freme.common.conversion.rdf.RDFConversionService;
 import eu.freme.common.conversion.rdf.RDFSerializationFormats;
@@ -14,12 +15,15 @@ import eu.freme.common.exception.FREMEHttpException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -30,6 +34,8 @@ import java.util.TreeMap;
 public class PostprocessingFilter implements Filter {
 
     private Logger logger = Logger.getLogger(PostprocessingFilter.class);
+
+    public final String HEADER_SECURITY_TOKEN = "X-Auth-Token";
 
     @Autowired
     ExceptionHandlerService exceptionHandlerService;
@@ -54,73 +60,88 @@ public class PostprocessingFilter implements Filter {
             HttpServletRequest httpRequest = (HttpServletRequest) req;
             HttpServletResponse httpResponse = (HttpServletResponse) res;
 
-            // get requested format of response
-            String outTypeString = httpRequest.getParameter("outformat");
-            if(Strings.isNullOrEmpty(outTypeString))
-                outTypeString = httpRequest.getParameter("o");
-            if(Strings.isNullOrEmpty(outTypeString) && !httpRequest.getHeader("Accept").equals("*/*"))
-                outTypeString = httpRequest.getHeader("Accept").split(";")[0];
+            String responseContent = null;
+            int responseStatus = HttpStatus.OK.value();
+            String responseContentType = RDFConstants.RDFSerialization.JSON.contentType();
 
-            RDFConstants.RDFSerialization outType = RDFConstants.RDFSerialization.CSV;
-            if (!Strings.isNullOrEmpty(outTypeString)) {
-                outType = rdfSerializationFormats.get(outTypeString);
-                if(outType == null)
-                    throw new BadRequestException("Can not use filter: " + req.getParameter("filter") + " with outformat/Accept-header: " + httpRequest.getParameter("outformat") + "/" + httpRequest.getHeader("Accept"));
+            String filterUrl = "/toolbox/convert/documents/" + req.getParameter("filter");
 
-            }
-
-            // set Accept header for original request to turtle
-            Map<String, String[]> extraParams = new TreeMap<>();
-            // delete outformat parameter
-            extraParams.put("outformat", null);//new String[]{"turtle"});
-            Map<String, String[]> extraHeaders = new TreeMap<>();
-            extraHeaders.put("Accept", new String[]{RDFConstants.RDFSerialization.TURTLE.contentType()});
-            HttpServletRequest wrappedRequest = new ModifiableParametersWrappedRequest(httpRequest, extraParams,extraHeaders);
-
-            // wrap the response to allow later modification
-            AccessibleHttpServletResponseWrapper wrappedResponse = new AccessibleHttpServletResponseWrapper(httpResponse);
-
-            chain.doFilter(wrappedRequest, wrappedResponse);
-
-
-            String responseContent = new String(wrappedResponse.getDataStream());
-            String responseContentType;
-
-            //// manipulate responseContent here
             try {
-                String baseUrl = String.format("%s://%s:%d", httpRequest.getScheme(), httpRequest.getServerName(), httpRequest.getServerPort());
+                // get requested format of response
+                String outTypeString = httpRequest.getParameter("outformat");
+                if(Strings.isNullOrEmpty(outTypeString))
+                    outTypeString = httpRequest.getParameter("o");
+                if(Strings.isNullOrEmpty(outTypeString) && !httpRequest.getHeader("Accept").equals("*/*"))
+                    outTypeString = httpRequest.getHeader("Accept").split(";")[0];
 
-                HttpResponse<String> response = Unirest
-                        .post(baseUrl + "/toolbox/convert/documents/"+req.getParameter("filter"))
-                        .header("Content-Type", RDFConstants.RDFSerialization.TURTLE.contentType())
-                        .header("Accept", outType.contentType())
-                        .body(responseContent)
-                        .asString();
+                RDFConstants.RDFSerialization outType = RDFConstants.RDFSerialization.CSV;
+                if (!Strings.isNullOrEmpty(outTypeString)) {
+                    outType = rdfSerializationFormats.get(outTypeString);
+                    if(outType == null)
+                        throw new BadRequestException("Can not use filter: " + req.getParameter("filter") + " with outformat = \"" + httpRequest.getParameter("outformat") + "\" / accept-header = \"" + httpRequest.getHeader("Accept")+"\". Ensure, that either outformat or accept header contains a valid value!");
 
-                if (response.getStatus() != HttpStatus.OK.value()) {
-                    throw new FREMEHttpException(
-                            "Postprocessing filter failed with status code: "
-                                    + response.getStatus() + " (" + response.getStatusText() + ")",
-                            HttpStatus.valueOf(response.getStatus()));
                 }
 
-                responseContent = response.getBody();
-                responseContentType = response.getHeaders().getFirst("Content-Type");
+                // set Accept header for original request to turtle
+                Map<String, String[]> extraParams = new TreeMap<>();
+                // delete outformat parameter
+                extraParams.put("outformat", null);//new String[]{"turtle"});
+                Map<String, String[]> extraHeaders = new TreeMap<>();
+                extraHeaders.put("Accept", new String[]{RDFConstants.RDFSerialization.TURTLE.contentType()});
+                HttpServletRequest wrappedRequest = new ModifiableParametersWrappedRequest(httpRequest, extraParams,extraHeaders);
 
-            } catch (UnirestException e) {
-                throw new FREMEHttpException(e.getMessage());
+                // wrap the response to allow later modification
+                AccessibleHttpServletResponseWrapper wrappedResponse = new AccessibleHttpServletResponseWrapper(httpResponse);
+
+                chain.doFilter(wrappedRequest, wrappedResponse);
+
+                String originalResponseContent = new String(wrappedResponse.getDataStream());
+
+                // postprocess only, if original request was successful
+                if(wrappedResponse.getStatus() != HttpStatus.OK.value()){
+                    responseContent = originalResponseContent;
+                    responseStatus = wrappedResponse.getStatus();
+                }else {
+
+                    //// manipulate originalResponseContent here
+                    String baseUrl = String.format("%s://%s:%d", httpRequest.getScheme(), httpRequest.getServerName(), httpRequest.getServerPort());
+
+                     HttpRequestWithBody filterRequest = Unirest
+                            .post(baseUrl + filterUrl)
+                            .header("Content-Type", RDFConstants.RDFSerialization.TURTLE.contentType())
+                            .header("Accept", outType.contentType());
+
+                    String token = httpRequest.getHeader(HEADER_SECURITY_TOKEN);
+                    if(!Strings.isNullOrEmpty(token))
+                        filterRequest = filterRequest.header(HEADER_SECURITY_TOKEN, httpRequest.getHeader(HEADER_SECURITY_TOKEN));
+
+                    HttpResponse<String> response = filterRequest
+                            .body(originalResponseContent)
+                            .asString();
+
+                    if (response.getStatus() == HttpStatus.OK.value()) {
+                        responseContentType = response.getHeaders().getFirst("Content-Type");
+                    }
+
+                    responseContent = response.getBody();
+                    responseStatus = response.getStatus();
+                }
+
+            } catch (Exception e) {
+                //exceptionHandlerService.writeExceptionToResponse((HttpServletRequest) req, (HttpServletResponse) res, e);
+                ResponseEntity<String> responseEntity = exceptionHandlerService.handleError((HttpServletRequest) req, e);
+                responseStatus = responseEntity.getStatusCode().value();
+                responseContent = responseEntity.getBody();
+                //httpResponse.flushBuffer();
             }
-            ////
 
             byte[] responseToSend = responseContent.getBytes();
 
             httpResponse.setContentType(responseContentType);
             httpResponse.setContentLength(responseToSend.length);
+            httpResponse.setStatus(responseStatus);
 
-            ServletOutputStream outputStream = res.getOutputStream();
-            outputStream.write(responseToSend);
-            outputStream.flush();
-            outputStream.close();
+            httpResponse.getWriter().write(responseContent);
 
         }
     }
