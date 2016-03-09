c16f6309ec37708db9fa8cae506ffd52f96009b5
Arne Binder <arne.b.binder@gmail.com>
2016-03-09 15:29:42 +0100

    switched to parent version: 0.2-SNAPSHOT, because there are no 0.1 versions of bservice submodules in the freme repo and created NifConverterController

---
+++ controllers/nifconverter/src/main/java/eu/freme/bservices/controllers/nifconverter/NifConverterController.java
@@ -0,0 +1,9 @@
+package eu.freme.bservices.controllers.nifconverter;
+
+import eu.freme.common.rest.BaseRestController;
+
+/**
+ * Created by Arne Binder (arne.b.binder@gmail.com) on 09.03.2016.
+ */
+public class NifConverterController extends BaseRestController {
+}
