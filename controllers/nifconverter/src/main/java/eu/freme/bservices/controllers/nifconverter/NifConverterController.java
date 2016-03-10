d381d6f96c87b7e18d8a3a8818b4c501dca0b46e
ArneBinder <arne.b.binder@gmail.com>
2016-03-10 01:26:17 +0100

    implemented #29 (without tests)

--- controllers/nifconverter/src/main/java/eu/freme/bservices/controllers/nifconverter/NifConverterController.java
+++ controllers/nifconverter/src/main/java/eu/freme/bservices/controllers/nifconverter/NifConverterController.java
@@ -1,10 +1,12 @@
 package eu.freme.bservices.controllers.nifconverter;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDF;
 import eu.freme.common.conversion.rdf.RDFConstants;
 import eu.freme.common.conversion.rdf.RDFConversionService;
 import eu.freme.common.exception.BadRequestException;
-import eu.freme.common.exception.ExceptionHandlerService;
 import eu.freme.common.rest.BaseRestController;
 import eu.freme.common.rest.NIFParameterSet;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,6 @@ public class NifConverterController extends BaseRestController {
     @Autowired
     RDFConversionService rdfConversionService;
 
-    @Autowired
-    ExceptionHandlerService exceptionHandlerService;
-
     @RequestMapping(value = "/toolbox/nif-converter", method = RequestMethod.POST)
     public ResponseEntity<String> convert(
             @RequestHeader(value = "Accept", required = false) String acceptHeader,
@@ -33,23 +32,29 @@ public class NifConverterController extends BaseRestController {
             @RequestBody String postBody,
             @RequestParam Map<String, String> allParams) {
 
-        NIFParameterSet nifParameters = this.normalizeNif(postBody,
-                acceptHeader, contentTypeHeader, allParams, false);
+        NIFParameterSet nifParameters = this.normalizeNif(postBody, acceptHeader, contentTypeHeader, allParams, false);
 
-        Model inModel = null;
+        Model model;
         try {
             if (nifParameters.getInformat().equals(RDFConstants.RDFSerialization.PLAINTEXT)) {
-                // TODO: implement model creation from plaintext
+                model = ModelFactory.createDefaultModel();
+                Resource strRes = model.createResource(nifParameters.getPrefix()+"#char=0,"+nifParameters.getInput().length());
+                strRes.addProperty(RDF.type, model.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#Context"));
+                strRes.addProperty(RDF.type, model.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#RFC5147String"));
+                strRes.addProperty(RDF.type, model.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#String"));
+                strRes.addProperty(RDF.type, model.createResource("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#Phrase"));
+                strRes.addLiteral(model.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#anchorOf"), nifParameters.getInput());
+                strRes.addLiteral(model.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#beginIndex"), 0);
+                strRes.addLiteral(model.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#endIndex"), nifParameters.getInput().length());
+                strRes.addProperty(model.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#referenceContext"), strRes);
+                strRes.addLiteral(model.createProperty("http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#isString"), nifParameters.getInput());
             }else {
-                inModel = rdfConversionService.unserializeRDF(nifParameters.getInput(), nifParameters.getInformat());
+                model = rdfConversionService.unserializeRDF(nifParameters.getInput(), nifParameters.getInformat());
             }
             HttpHeaders responseHeaders = new HttpHeaders();
-            String serialization = rdfConversionService.serializeRDF(inModel,
-                    nifParameters.getOutformat());
-            responseHeaders.add("Content-Type", nifParameters.getOutformat()
-                    .contentType());
-            return new ResponseEntity<>(serialization, responseHeaders,
-                    HttpStatus.OK);
+            String serialization = rdfConversionService.serializeRDF(model, nifParameters.getOutformat());
+            responseHeaders.add("Content-Type", nifParameters.getOutformat().contentType());
+            return new ResponseEntity<>(serialization, responseHeaders, HttpStatus.OK);
         } catch (Exception e) {
             throw new BadRequestException(e.getMessage());
         }
