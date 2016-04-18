a2f04f67db0fd19686dee933268d61b3a1ecfec4
Arne Binder <arne.b.binder@gmail.com>
2016-04-18 18:35:06 +0200

    implemented #38

--- cloud/load-balancer/src/main/java/eu/freme/bservices/cloud/loadbalancer/LoadBalancingProxyConfig.java
+++ cloud/load-balancer/src/main/java/eu/freme/bservices/cloud/loadbalancer/LoadBalancingProxyConfig.java
@@ -2,10 +2,39 @@ package eu.freme.bservices.cloud.loadbalancer;
 
 public class LoadBalancingProxyConfig {
 
+	public enum PropertyName {
+		LOCAL_ENDPOINT("local-endpoint"),
+		TARGET_ENDPOINT("target-endpoint"),
+		SERVICE_NAME("service-name");
+		private String value;
+		PropertyName(String key){
+			this.value = key;
+		}
+		String getValue(){return value;}
+	}
+
 	String localEndpoint;
 	String targetEndpoint;
 	String serviceName;
-	
+
+	public LoadBalancingProxyConfig(){}
+
+	public void setProperty(String propertyName, String value){
+		if(PropertyName.LOCAL_ENDPOINT.getValue().equals(propertyName))
+			localEndpoint = value;
+		else if(PropertyName.TARGET_ENDPOINT.getValue().equals(propertyName))
+			targetEndpoint = value;
+		else if(PropertyName.SERVICE_NAME.getValue().equals(propertyName))
+			serviceName = value;
+		else{
+			throw new RuntimeException("unknown parameter: "+ propertyName);
+		}
+	}
+
+	public boolean isValid(){
+		return localEndpoint!=null && targetEndpoint!=null && serviceName!=null;
+	}
+
 	public LoadBalancingProxyConfig(String localEndpoint,
 			String targetEndpoint, String serviceName) {
 		super();
